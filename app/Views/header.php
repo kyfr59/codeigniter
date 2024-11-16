5d61e77f9121595d5497cfae9745372bd3c13760
dupont-ext <kyfr59@gmail.com>
2024-11-16 08:31:28 +0100

    Ajout du lien de connexion/deconnexion dans le header

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,3 +1,9 @@
+
+<?php if ($user = auth()->user()): ?>
+    <a href="<?= url_to('logout') ?>"><?= lang('Auth.logout') ?></a></p>
+<?php else: ?>
+    <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
+<?php endif ?>
 <link rel="stylesheet" href="css/index.min.css">
 <script src="js/index.min.js"></script>
 <h1>HEADER</h1>
